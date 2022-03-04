3117b58f44e21ef98ab1a6c3a75a6987fab2cb28
Omkar <omkarv9@gmail.com>
2022-03-04 14:20:29 +0530

    N

---
+++ src/components/LoginPage.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import pic1 from "../assets/ShoppingGirl.jpeg"
+import pic2 from "../assets/LOGO.jpeg"
+import pic3 from "../assets/gogface.png"
+
+import '../App.css'
+import { render } from 'react-dom'
+
+export default function SignInPage() {
+
+    return (
+        
+        <div className="text-center auto">
+
+        
+            <img src={pic1} align="left"  alt="pic" width={500} height={750} />
+            <img src={pic2} align="left"/>
+            <h2  style={{fontFamily: "Gilroy"}}>Sign in</h2>
+            {/* <img src={pic3} align="left"/> */}
+            <form >
+                <p>
+                    <label style={{fontFamily: "Gilroy"}}>Username</label><br/>
+                    <input type="text" name="first_name" required />
+                </p>
+                <p>
+                    <label style={{fontFamily: "Gilroy"}}>Password</label>
+                    <br/>
+                    <input type="password" name="password" required />
+                </p>
+                <p>
+                    <button id="sub_btn" type="submit" style={{fontFamily: "Gilroy"}}>Login</button>
+                </p>
+            </form>
+            <footer>
+                <p style={{fontFamily: "Gilroy"}}><Link to="/register">Forget password?</Link></p>
+                <p style={{fontFamily: "Gilroy"}}>First time? <Link to="/register">Create an account</Link></p>
+                <p style={{fontFamily: "Gilroy"}}><Link to="/">Back to Homepage</Link></p>
+            </footer>
+        </div>
+    );
+}
