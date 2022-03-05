28b5009f5b6e019bc8af57c0207b00f0ba470de1
Omkar <omkarv9@gmail.com>
2022-03-06 00:41:10 +0530

    Initial commit

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import item from '../assets/item.png'
+import line from '../assets/line.png'
 
 
 export default function ProductDetails() {
     return(
         <div style={{background:"#C8F2EF"}}>
-            <div style={{position:"absolute",left:"3%",top:"2%"}}>
+            <div style={{position:"absolute",left:"4%",top:"8%"}}>
                 <img src={item} alt='product' width={385} height={510}/>
             </div>
             {/* <div className='pdetails'>
@@ -24,28 +25,51 @@ export default function ProductDetails() {
                     Description
                 </div>
             </div> */}
-            <div style={{fontFamily:"Calibri" ,position:"absolute", left:"3%"}}>
+            <div style={{fontStyle:"italic" ,position:"absolute", right:"3%"}}>
+                <p style={{fontWeight:"bold"}}>HRX by Hrithik Roshan</p>
+                <p style={{color:"GrayText"}}>HRX By Hrithik Roshan Running Men Blue Packable Solid Jacket</p>
+                <img src={line} width={600}/>
+                <div><p>Rs. 1664 <p>(65% OFF)</p></p></div>
+                <p>inclusive of all taxes</p>
+                {/* line */}
+                <p>SELECT SIZE</p>
+                {/* SIZES */}
+                {/* BUTTONS */}
+                {/* line */}
+                <p>Get it by Sat, Feb 05 - 462023<br></br>
+                Seller: Omnitech Retail </p>
+                {/* line */}
+                <p>DELIVERY OPTIONS</p>
+                <p>Get it by Sat, Feb 05<br></br>
+                    Pay on delivery available<br></br>
+                    Easy 15 days return & exchange available</p>
+                {/* line */}
+                <p>BEST OFFERS</p>
+                <p>This product is already a its best price</p>
+                <p>EMI option available</p>
+                <p>EMI starting from Rs.77/month</p>
+                {/* line */}
                 <p>PRODUCT DETAILS</p>
                 <p>When you enjoy your morning run, you need to have the right gear.<br></br>
-Cut from fabric enhanced with Rapid-Dry and Anti Microbial technology,<br></br>
-the HRX Men's Running Jacket keeps you cool and comfortable throughout<br></br>
-your run. What's more, it's got discreet storage in the back.</p>
-                <p>FEATURES</p>
-                <p>Running Jacket
-Rapid-Dry Technology wicks sweat & makes the fabric dry fast.<br></br>
-Anti-microbial technology prevents the growth of odor-causing microbes keeping<br></br>
-you fresh & hygienic all day.<br></br>
-Adjustable hood protects your ears and neck when you step outdoors.<br></br>
-Waterproof fabric keeps you cool and dry when it rains.<br></br>
-Pocket at the back to hold the essentials.<br></br>
-Folds neatly into a small pack<br></br>
-Front open<br></br>
-Lightweight Jacket<br></br>
-Neck: Hooded<br></br>
-Sleeve length: Full Sleeves<br></br>
-Design: Solid<br></br>
-Length: Regular length<br></br>
-Fit: Regular fit</p>
+                Cut from fabric enhanced with Rapid-Dry and Anti Microbial technology,<br></br>
+                the HRX Men's Running Jacket keeps you cool and comfortable throughout<br></br>
+                your run. What's more, it's got discreet storage in the back.</p>
+                                <p>FEATURES</p>
+                                <p>Running Jacket
+                Rapid-Dry Technology wicks sweat & makes the fabric dry fast.<br></br>
+                Anti-microbial technology prevents the growth of odor-causing microbes keeping<br></br>
+                you fresh & hygienic all day.<br></br>
+                Adjustable hood protects your ears and neck when you step outdoors.<br></br>
+                Waterproof fabric keeps you cool and dry when it rains.<br></br>
+                Pocket at the back to hold the essentials.<br></br>
+                Folds neatly into a small pack<br></br>
+                Front open<br></br>
+                Lightweight Jacket<br></br>
+                Neck: Hooded<br></br>
+                Sleeve length: Full Sleeves<br></br>
+                Design: Solid<br></br>
+                Length: Regular length<br></br>
+                Fit: Regular fit</p>
             </div>
         </div>
     )
