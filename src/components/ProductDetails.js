e8f87c1c30af124232a78188ac2aa8c7705fb7ef
Omkar <omkarv9@gmail.com>
2022-03-08 10:42:06 +0530

    Initial commit

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,54 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import item from '../assets/item.png'
-import line from '../assets/line.png'
+import line from '../assets/Line 12.png'
+import size from '../assets/Ellipse 25.png'
 
 
 export default function ProductDetails() {
     return(
         <div style={{background:"#C8F2EF"}}>
             <div style={{position:"absolute",left:"4%",top:"8%"}}>
-                <img src={item} alt='product' width={385} height={510}/>
+                <img src={item} alt='product' width={430} height={510}/>
             </div>
-            {/* <div className='pdetails'>
-                <div style={{position:"relative",top:"-20%"}}>
-                <strong>Product Name</strong>
-                </div>
-                <div style={{position:"relative",left:"-24.5%",top:"-12.5%"}}>
-                    <strong>Price</strong>
-                </div>
-                <div style={{display:"flex",msFlexDirection:"row",left:"-10%"}}>
-                    <input type='button' id= 'buy' value='Buy Now' onClick={() => window.open('./payment','_self')}></input>
-                    <input type='button' id= 'cart' value='Add to Cart' onClick={() => window.open('./cart','_self')}></input>
-                </div>
-                <div style={{position:"relative",left:"-47%",top:"15%"}}>
-                    Description
-                </div>
-            </div> */}
             <div style={{fontStyle:"italic" ,position:"absolute", right:"3%"}}>
                 <p style={{fontWeight:"bold"}}>HRX by Hrithik Roshan</p>
                 <p style={{color:"GrayText"}}>HRX By Hrithik Roshan Running Men Blue Packable Solid Jacket</p>
-                <img src={line} width={600}/>
-                <div><p>Rs. 1664 <p>(65% OFF)</p></p></div>
-                <p>inclusive of all taxes</p>
-                {/* line */}
+                <img src={line} />
+                <p>Rs. 1664 <span style={{color:"orange"}}>(65% OFF)</span></p>
+                <p style={{color:"green"}}>inclusive of all taxes</p>
+                <img src={line} />
                 <p>SELECT SIZE</p>
-                {/* SIZES */}
+                <img src={size}  style={{position:"relative"}}/> <p style={{position:"absolute",left:"2.5%",top:"20%",fontSize:"25px"}}>S</p>
+                <img src={size}  style={{position:"relative" ,left:"2%"}}/> <p style={{position:"absolute",left:"13%",top:"20%",fontSize:"25px"}}>M</p>
+                <img src={size}  style={{position:"relative",left:"4%"}}/> <p style={{position:"absolute",left:"25%",top:"20%",fontSize:"25px"}}>L</p>
+                <img src={size}  style={{position:"relative",left:"6%"}}/> <p style={{position:"absolute",left:"35%",top:"20%",fontSize:"25px"}}>XL</p>
+                <img src={size}  style={{position:"relative",left:"8%"}}/> <p style={{position:"absolute",left:"45%",top:"20%",fontSize:"25px"}}>XXL</p>
                 {/* BUTTONS */}
-                {/* line */}
+                <img src={line} style={{position:"absolute",top:"25%",left:"0%"}}/>
                 <p>Get it by Sat, Feb 05 - 462023<br></br>
                 Seller: Omnitech Retail </p>
-                {/* line */}
+                <img src={line} />
                 <p>DELIVERY OPTIONS</p>
                 <p>Get it by Sat, Feb 05<br></br>
                     Pay on delivery available<br></br>
                     Easy 15 days return & exchange available</p>
-                {/* line */}
+                    <img src={line} />
                 <p>BEST OFFERS</p>
                 <p>This product is already a its best price</p>
                 <p>EMI option available</p>
                 <p>EMI starting from Rs.77/month</p>
-                {/* line */}
+                <img src={line} />
                 <p>PRODUCT DETAILS</p>
                 <p>When you enjoy your morning run, you need to have the right gear.<br></br>
                 Cut from fabric enhanced with Rapid-Dry and Anti Microbial technology,<br></br>
