340dfa2fc5f641deaab505d10dd408456e6cda20
Omkar <omkarv9@gmail.com>
2022-03-09 10:26:42 +0530

    Initial commit

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -3,47 +3,67 @@ import { Link } from 'react-router-dom'
 import item from '../assets/item.png'
 import line from '../assets/Line 12.png'
 import size from '../assets/Ellipse 25.png'
+import bag from '../assets/Icon awesome-shopping-bag.png'
+import like from '../assets/Icon feather-heart.png'
+import deliver from '../assets/Icon awesome-car.png'
+import card from '../assets/Icon material-payment.png'
+import arrows from '../assets/arrows.png'
+import add from '../assets/Group 15.png'
 
 
 export default function ProductDetails() {
     return(
-        <div style={{background:"#C8F2EF"}}>
+        <div style={{backgroundColor:"white"}}>
             <div style={{position:"absolute",left:"4%",top:"8%"}}>
-                <img src={item} alt='product' width={430} height={510}/>
+                <img src={item} alt='product' width={500} height={600}/>
             </div>
             <div style={{fontStyle:"italic" ,position:"absolute", right:"3%"}}>
+
                 <p style={{fontWeight:"bold"}}>HRX by Hrithik Roshan</p>
                 <p style={{color:"GrayText"}}>HRX By Hrithik Roshan Running Men Blue Packable Solid Jacket</p>
                 <img src={line} />
+
                 <p>Rs. 1664 <span style={{color:"orange"}}>(65% OFF)</span></p>
                 <p style={{color:"green"}}>inclusive of all taxes</p>
                 <img src={line} />
+
                 <p>SELECT SIZE</p>
+                {/* SIZES */}
                 <img src={size}  style={{position:"relative"}}/> <p style={{position:"absolute",left:"2.5%",top:"20%",fontSize:"25px"}}>S</p>
                 <img src={size}  style={{position:"relative" ,left:"2%"}}/> <p style={{position:"absolute",left:"13%",top:"20%",fontSize:"25px"}}>M</p>
                 <img src={size}  style={{position:"relative",left:"4%"}}/> <p style={{position:"absolute",left:"25%",top:"20%",fontSize:"25px"}}>L</p>
                 <img src={size}  style={{position:"relative",left:"6%"}}/> <p style={{position:"absolute",left:"35%",top:"20%",fontSize:"25px"}}>XL</p>
                 <img src={size}  style={{position:"relative",left:"8%"}}/> <p style={{position:"absolute",left:"45%",top:"20%",fontSize:"25px"}}>XXL</p>
+
                 {/* BUTTONS */}
-                <img src={line} style={{position:"absolute",top:"25%",left:"0%"}}/>
+                <button id="sub_btn" type="submit" style={{fontStyle:"italic",background:"red",width:"232px",height:"60px",fontSize:"17px"}}>ADD TO CART</button>
+                <img src={bag}/>
+                <button id="sub_btn" type="submit" style={{fontStyle:"italic",background:"red",width:"232px",height:"60px",fontSize:"17px"}}>WISHLIST</button>
+                <img src={like} style={{position:"relative"}}/>
+
+                <img src={line} style={{position:"absolute",top:"24.3%",left:"0%"}}/>
                 <p>Get it by Sat, Feb 05 - 462023<br></br>
                 Seller: Omnitech Retail </p>
                 <img src={line} />
+
                 <p>DELIVERY OPTIONS</p>
-                <p>Get it by Sat, Feb 05<br></br>
-                    Pay on delivery available<br></br>
-                    Easy 15 days return & exchange available</p>
-                    <img src={line} />
+                <img src={deliver}/><p>Get it by Sat, Feb 05<br></br>
+                <img src={card}/>Pay on delivery available<br></br>
+                <img src={arrows} style={{width:"40px" ,height:"40px"}}/>Easy 15 days return & exchange available</p>
+                <img src={line} />
+
                 <p>BEST OFFERS</p>
                 <p>This product is already a its best price</p>
                 <p>EMI option available</p>
                 <p>EMI starting from Rs.77/month</p>
                 <img src={line} />
+
                 <p>PRODUCT DETAILS</p>
                 <p>When you enjoy your morning run, you need to have the right gear.<br></br>
                 Cut from fabric enhanced with Rapid-Dry and Anti Microbial technology,<br></br>
                 the HRX Men's Running Jacket keeps you cool and comfortable throughout<br></br>
                 your run. What's more, it's got discreet storage in the back.</p>
+
                                 <p>FEATURES</p>
                                 <p>Running Jacket
                 Rapid-Dry Technology wicks sweat & makes the fabric dry fast.<br></br>
@@ -61,6 +81,8 @@ export default function ProductDetails() {
                 Length: Regular length<br></br>
                 Fit: Regular fit</p>
             </div>
+
+            <img src={add} style={{position:"absolute",width:"90px",height:"90px",left:"17.5%",top:"90%"}}/>
         </div>
     )
 }
